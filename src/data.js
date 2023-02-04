6ad5827ff960800d37605d8a1585b8717638cb1a
unknown <viczik16@gmail.com>
2023-02-04 17:59:00 +0100

    added shot

--- src/data.js
+++ src/data.js
@@ -1,23 +1,7 @@
 export default [
-    {
-        id : 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        price: 125,
-        stats: {
-            rating: 5.0,
-            reviewCount : 30
-        },
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-        openspot: 0
-    },
 
     {
-        id : 2,
+        id : 1,
         title: "Sydney Opera House",
         location: "Australia",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -34,7 +18,7 @@ export default [
     },
 
     {
-        id : 3,
+        id : 2,
         title: "Geirangerfjord",
         location: "Norway",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -50,5 +34,22 @@ export default [
         openspot: 2
     },
 
+
+    {
+        id : 3,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        price: 125,
+        stats: {
+            rating: 5.0,
+            reviewCount : 30
+        },
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+        openspot: 0
+    },
    
 ]
